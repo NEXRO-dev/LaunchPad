c24d49e922971a2d15b022ae0d856966d9dfa049
NEXRO-dev <nexro0424@gmail.com>
2026-01-17 14:16:52 +0900

    Fix api import path to use dist

--- packages/server/api/index.ts
+++ packages/server/api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { buildApp } from '../src/app.js';
+import { buildApp } from '../dist/app.js';
 
 // Cache the instance
 let app: any;
